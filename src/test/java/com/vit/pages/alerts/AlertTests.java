7c8d5258dfd860be14ecdbb52ce6bb7b27863365
Vitaliy Rodyna <ukrdao@gmail.com>
2022-08-07 15:06:30 +0300

    added  code

---
+++ src/test/java/com/vit/pages/alerts/AlertTests.java
@@ -0,0 +1,39 @@
+package com.vit.pages.alerts;
+
+import com.vit.utils.base.BaseTests;
+import com.vit.pages.AlertsPage;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class AlertTests extends BaseTests {
+
+    @Test
+    public void testAcceptAlert(){
+        AlertsPage alertsPage = homePage.clickJavaScriptAlerts();
+       alertsPage.triggerAlert();
+       alertsPage.alert_clickToAccept();
+       assertEquals(alertsPage.getResult(), "You successfuly clicked an alert", "Results text incorrect");
+    }
+
+    @Test
+    public void testGetTextFromAlert(){
+        var alertsPage = homePage.clickJavaScriptAlerts();
+        alertsPage.triggerConfirm();
+        String text = alertsPage.alert_getText();
+        alertsPage.alert_clickToDismiss();
+        assertEquals(text, "I am a JS Confirm", "Alert text incorrect");
+    }
+
+    @Test
+    public void testSetInputInAlert(){
+        var alertsPage = homePage.clickJavaScriptAlerts();
+        alertsPage.triggerPrompt();
+
+        String text = "TAU rocks!";
+        alertsPage.alert_setInput(text);
+        alertsPage.alert_clickToAccept();
+        assertEquals(alertsPage.getResult(), "You entered: " + text, "Results text incorrect");
+    }
+}
