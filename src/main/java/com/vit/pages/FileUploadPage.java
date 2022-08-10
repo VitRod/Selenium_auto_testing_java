f41e0234bb11dfd83cadc43ab109fd2a09b7b0a1
Vitaliy Rodyna <ukrdao@gmail.com>
2022-08-10 08:49:24 +0300

    By inputField = By.id("file-upload");

--- src/main/java/com/vit/pages/FileUploadPage.java
+++ src/main/java/com/vit/pages/FileUploadPage.java
@@ -6,14 +6,14 @@ import org.openqa.selenium.WebDriver;
 public class FileUploadPage {
 	
 	private WebDriver driver;
-	private By inputField = By.id("file-upload");
+	
 	
 	
     
 	 
 
 	 
-   
+	private By inputField = By.id("file-upload");
 	private By uploadButton = By.id("file-submit");
 	private By uploadedFiles = By.id("uploaded-files");
 	 
