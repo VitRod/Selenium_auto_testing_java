d7bc03b9cc57632e2f4b3afd5512dac091a94f23
Vitaliy Rodyna <ukrdao@gmail.com>
2022-08-10 16:51:53 +0300

    editorPage.decreaseIndention();
            editorPage.setTextArea(text2);

--- src/test/java/vit/com/pages/frames/FrameTests.java
+++ src/test/java/vit/com/pages/frames/FrameTests.java
@@ -16,11 +16,11 @@ public class FrameTests extends BaseTests {
         String text2 = "world";
 
         editorPage.setTextArea(text1);
-
-    
         editorPage.decreaseIndention();
         editorPage.setTextArea(text2);
 
+    
+        
         assertEquals(editorPage.getTextFromEditor(), text1+text2, "Text from editor is incorrect");
     }
 }
