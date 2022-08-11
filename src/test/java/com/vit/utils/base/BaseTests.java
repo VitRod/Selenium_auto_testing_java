467b247348e2f4a95dea5dc4dbfbd66365887395
Vitaliy Rodyna <ukrdao@gmail.com>
2022-08-11 08:03:17 +0300

    var camera = (TakesScreenshot)driver;

--- src/test/java/com/vit/utils/base/BaseTests.java
+++ src/test/java/com/vit/utils/base/BaseTests.java
@@ -40,12 +40,12 @@ public class BaseTests {
     public void recordFailure(ITestResult result){
         if(ITestResult.FAILURE == result.getStatus())
         {
-            var camera = (TakesScreenshot)driver;
+           
             
             
        
     
-	
+        	 var camera = (TakesScreenshot)driver;
             File screenshot = camera.getScreenshotAs(OutputType.FILE);
             try{
                 Files.move(screenshot, new File("resources/screenshots/" + result.getName() + ".png"));
