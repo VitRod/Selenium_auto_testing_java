7e6e5dfc8b082a0c999be48832ea52c9d5f192a8
Vitaliy Rodyna <ukrdao@gmail.com>
2022-08-11 18:27:19 +0300

    var hoversPage =
     homePage.clickHovers();

--- src/test/java/com/vit/pages/hover/HoverTests.java
+++ src/test/java/com/vit/pages/hover/HoverTests.java
@@ -9,13 +9,13 @@ public class HoverTests extends BaseTests {
 
 	@Test
 	 public void testHoverUser1(){
-	
+		var hoversPage = homePage.clickHovers();
 	
 	
 	
 	
    
-        var hoversPage = homePage.clickHovers();
+        
         var caption = hoversPage.hoverOverFigure(1);
         assertTrue(caption.isCaptionDisplayed(), "Caption not displayed");
         assertEquals(caption.getTitle(), "name: user1", "Caption title incorrect");
