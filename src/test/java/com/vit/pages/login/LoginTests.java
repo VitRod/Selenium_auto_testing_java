ef632014be61b51d75f2222d49e69861a447caf0
Vitaliy Rodyna <ukrdao@gmail.com>
2022-08-12 11:28:08 +0300

    loginPage.setUsername("tomsmith");

--- src/test/java/com/vit/pages/login/LoginTests.java
+++ src/test/java/com/vit/pages/login/LoginTests.java
@@ -12,12 +12,12 @@ public class LoginTests extends BaseTests {
 	@Test
     public void testSuccessfulLogin(){
 		LoginPage loginPage = homePage.clickFormAuthentication();
-	
+		loginPage.setUsername("tomsmith");
 	
 	
     
         
-        loginPage.setUsername("tomsmith");
+        
         loginPage.setPassword("SuperSecretPassword!");
         SecureAreaPage secureAreaPage = loginPage.clickLoginButton();
         assertTrue(secureAreaPage.getAlertText()
