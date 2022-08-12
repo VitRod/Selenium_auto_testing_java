8765c12189552f3bed570ba73b29dbfef0d0a23e
Vitaliy Rodyna <ukrdao@gmail.com>
2022-08-12 15:08:17 +0300

    added

--- src/main/java/com/vit/pages/LoginPage.java
+++ src/main/java/com/vit/pages/LoginPage.java
@@ -25,7 +25,6 @@ public class LoginPage {
 	public SecureAreaPage clickLoginButton(){
         driver.findElement(loginButton).click();
         return new SecureAreaPage(driver);
-    
 	}
 }
     
