6469b525477cc37a9322e63bbcc176892ad5b997
Vitaliy Rodyna <ukrdao@gmail.com>
2022-08-13 12:55:04 +0300

    WebDriverWait wait =
    new WebDriverWait(driver,5);

--- src/main/java/com/vit/pages/DynamicLoadingExample1Page.java
+++ src/main/java/com/vit/pages/DynamicLoadingExample1Page.java
@@ -22,9 +22,9 @@ public class DynamicLoadingExample1Page {
 
     public void clickStart(){
     	driver.findElement(startButton).click();
-   
+    	WebDriverWait wait = new WebDriverWait(driver, 5);
         
-        WebDriverWait wait = new WebDriverWait(driver, 5);
+        
         wait.until(ExpectedConditions.invisibilityOf(
                 driver.findElement(loadingIndicator)));
 
