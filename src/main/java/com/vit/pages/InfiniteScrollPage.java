f2490039d61b8a4d3bbd9c408e660932d3e7acd5
Vitaliy Rodyna <ukrdao@gmail.com>
2022-08-13 06:06:30 +0300

    comments

--- src/main/java/com/vit/pages/InfiniteScrollPage.java
+++ src/main/java/com/vit/pages/InfiniteScrollPage.java
@@ -13,12 +13,13 @@ public class InfiniteScrollPage {
         this.driver = driver;
     }
 
-    
-
-    /**
+	/**
      * Scrolls until paragraph with index specified is in view
      * @param index 1-based
      */
+	
+
+    
     public void scrollToParagraph(int index){
         String script = "window.scrollTo(0, document.body.scrollHeight)";
         JavascriptExecutor jsExecutor = (JavascriptExecutor)driver;
