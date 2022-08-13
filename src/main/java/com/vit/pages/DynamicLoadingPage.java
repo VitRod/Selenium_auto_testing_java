672000c191dae6ef5bb85ebe00d7f07ace1d04be
Vitaliy Rodyna <ukrdao@gmail.com>
2022-08-13 10:24:18 +0300

    String linkXpath_Format =
    ".//a[contains(text(), '%s')]"

--- src/main/java/com/vit/pages/DynamicLoadingPage.java
+++ src/main/java/com/vit/pages/DynamicLoadingPage.java
@@ -7,11 +7,11 @@ import org.openqa.selenium.interactions.Actions;
 
 public class DynamicLoadingPage {
 
+	private WebDriver driver;
+	private String linkXpath_Format = ".//a[contains(text(), '%s')]";
 	
-	
-	
-    private WebDriver driver;
-    private String linkXpath_Format = ".//a[contains(text(), '%s')]";
+    
+    
     private By link_Example1 = By.xpath(String.format(linkXpath_Format, "Example 1"));
     private By link_Example2 = By.xpath(String.format(linkXpath_Format, "Example 2"));
 
