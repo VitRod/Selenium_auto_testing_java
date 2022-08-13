6043a9d038447d1037d641e39b789c54eb03d396
Vitaliy Rodyna <ukrdao@gmail.com>
2022-08-13 09:10:40 +0300

    added files  for  navigation page

---
+++ src/test/java/com/vit/pages/navigation/NavigationTests.java
@@ -0,0 +1,25 @@
+package com.vit.pages.navigation;
+
+import com.vit.utils.base.BaseTests;
+import org.testng.annotations.Test;
+
+public class NavigationTests extends BaseTests {
+
+    // this code to demonstrate window management
+ 
+
+    @Test
+    public void testNavigator(){
+        homePage.clickDynamicLoading().clickExample1();
+        getWindowManager().goBack();
+        getWindowManager().refreshPage();
+        getWindowManager().goForward();
+        getWindowManager().goTo("https://google.com");
+    }
+
+    @Test
+    public void testSwitchTab(){
+        homePage.clickMultipleWindows().clickHere();
+        getWindowManager().switchToTab("New Window");
+    }
+}
