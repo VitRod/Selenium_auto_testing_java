97471e9d4dd4e2114c52bab3c6551a077b6b9fa8
Vitaliy Rodyna <ukrdao@gmail.com>
2022-08-13 09:50:34 +0300

    driver.findElement(startButton).click();

--- src/main/java/com/vit/pages/DynamicLoadingExample2Page.java
+++ src/main/java/com/vit/pages/DynamicLoadingExample2Page.java
@@ -16,11 +16,11 @@ public class DynamicLoadingExample2Page {
     }
     
 	 public void clickStart() {
-
+		 driver.findElement(startButton).click();
     
 
    
-        driver.findElement(startButton).click();
+       
         WebDriverWait wait = new WebDriverWait(driver, 5);
         wait.until(ExpectedConditions.presenceOfElementLocated(loadedText));
     }
