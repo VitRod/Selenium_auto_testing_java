1fd4aa21a6dc31fd5bdc7614e7e1d8e90b749375
Vitaliy Rodyna <ukrdao@gmail.com>
2022-08-14 14:23:25 +0300

    public ForgotPasswordPage(WebDriver driver){
            this.driver = driver;
        }

--- src/main/java/com/vit/pages/ForgotPasswordPage.java
+++ src/main/java/com/vit/pages/ForgotPasswordPage.java
@@ -9,11 +9,11 @@ public class ForgotPasswordPage {
     private By emailField = By.id("email");
     private By retrievePasswordButton = By.id("form_submit");
     
-    
-
     public ForgotPasswordPage(WebDriver driver){
         this.driver = driver;
     }
+
+    
 
     public void enterEmail(String email){
         driver.findElement(emailField).sendKeys(email);
