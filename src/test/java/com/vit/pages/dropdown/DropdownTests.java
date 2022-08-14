27537d49df0d23fe841fb6784ad4f17c82b639a7
Vitaliy Rodyna <ukrdao@gmail.com>
2022-08-14 12:13:18 +0300

    assertEquals(selectedOptions.size(),
    1, "Incorrect number of selections");

--- src/test/java/com/vit/pages/dropdown/DropdownTests.java
+++ src/test/java/com/vit/pages/dropdown/DropdownTests.java
@@ -16,8 +16,8 @@ public class DropdownTests extends BaseTests {
 
     	dropDownPage.selectFromDropDown(option);
     	var selectedOptions = dropDownPage.getSelectedOptions();
+    	assertEquals(selectedOptions.size(), 1, "Incorrect number of selections");
         
-        assertEquals(selectedOptions.size(), 1, "Incorrect number of selections");
         assertTrue(selectedOptions.contains(option), "Option not selected");
     }
 }
