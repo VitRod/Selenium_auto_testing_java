afb4c629cc0017540655272334ba6e234020068d
Vitaliy Rodyna <ukrdao@gmail.com>
2022-08-14 16:06:02 +0300

    public EmailSentPage(WebDriver driver){
            this.driver = driver;
        }

--- src/main/java/com/vit/pages/EmailSentPage.java
+++ src/main/java/com/vit/pages/EmailSentPage.java
@@ -8,11 +8,11 @@ public class EmailSentPage {
 	private WebDriver driver;
 	private By contentArea = By.id("content");
     
-   
-
-    public EmailSentPage(WebDriver driver){
+	public EmailSentPage(WebDriver driver){
         this.driver = driver;
     }
+
+    
 
     public String getMessage(){
         return driver.findElement(contentArea).getText();
