357436a8e0737b5603bb1922ddb45097cc58ed3f
Vitaliy Rodyna <ukrdao@gmail.com>
2022-08-14 17:23:07 +0300

    String value = "4";
            var sliderPage = homePage.clickHorizonalSlider();

--- src/test/java/com/vit/pages/slider/SliderTests.java
+++ src/test/java/com/vit/pages/slider/SliderTests.java
@@ -9,10 +9,10 @@ public class SliderTests extends BaseTests {
 
 	@Test
     public void testSlideToWholeNumber(){
-	
-    
-        String value = "4";
+		String value = "4";
         var sliderPage = homePage.clickHorizonalSlider();
+    
+        
         sliderPage.setSliderValue(value);
         assertEquals(sliderPage.getSliderValue(), value, "Slider value is incorrect");
     }
