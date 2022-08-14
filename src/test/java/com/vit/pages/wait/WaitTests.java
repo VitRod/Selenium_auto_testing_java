cfc2db78dbe5e1398e436cf2b542ceb89c1f040b
Vitaliy Rodyna <ukrdao@gmail.com>
2022-08-14 13:04:52 +0300

    edited

--- src/test/java/com/vit/pages/wait/WaitTests.java
+++ src/test/java/com/vit/pages/wait/WaitTests.java
@@ -13,6 +13,6 @@ public class WaitTests extends BaseTests {
 		 loadingPage.clickStart();
 		 assertEquals(loadingPage.getLoadedText(), "Hello World!", "Loaded text incorrect");
 	}
-       
+}   
     
-}
+
