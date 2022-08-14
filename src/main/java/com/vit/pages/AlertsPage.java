49dc51d418ac17fcdcbe85f398edb7e4d1f6c7c7
Vitaliy Rodyna <ukrdao@gmail.com>
2022-08-14 09:07:05 +0300

    private WebDriver driver;

--- src/main/java/com/vit/pages/AlertsPage.java
+++ src/main/java/com/vit/pages/AlertsPage.java
@@ -5,7 +5,11 @@ import org.openqa.selenium.WebDriver;
 
 public class AlertsPage {
 
-    private WebDriver driver;
+	private WebDriver driver;
+	
+	
+	
+	
     private By triggerAlertButton = By.xpath(".//button[text()='Click for JS Alert']");
     private By triggerConfirmButton = By.xpath(".//button[text()='Click for JS Confirm']");
     private By triggerPromptButton = By.xpath(".//button[text()='Click for JS Prompt']");
