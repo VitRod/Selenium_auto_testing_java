7a8ec6df6c8d74c80b61882f733ebe5a05c4d244
Vitaliy Rodyna <ukrdao@gmail.com>
2022-08-14 10:49:28 +0300

    By.cssSelector("#login button");

--- src/main/java/com/vit/pages/LoginPage.java
+++ src/main/java/com/vit/pages/LoginPage.java
@@ -8,10 +8,10 @@ public class LoginPage {
 	private WebDriver driver;
 	private By usernameField = By.id("username");
 	private By passwordField = By.id("password");
-	
-	
-	
 	private By loginButton = By.cssSelector("#login button");
+	
+	
+	
     
 	public LoginPage(WebDriver driver){
         this.driver = driver;
