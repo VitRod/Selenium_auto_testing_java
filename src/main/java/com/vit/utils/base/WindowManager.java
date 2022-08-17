a521caf1e2fbb1d7ac8e6741b7bfd1836a24abff
Vitaliy Rodyna <ukrdao@gmail.com>
2022-08-17 08:29:35 +0300

    for(String window :windows){

--- src/main/java/com/vit/utils/base/WindowManager.java
+++ src/main/java/com/vit/utils/base/WindowManager.java
@@ -36,10 +36,10 @@ public class WindowManager {
 	        System.out.println("Window handles:");
 	        windows.forEach(System.out::println);
 	       
-	        
+	        for(String window : windows){
 	       
 	        
-	        for(String window : windows){
+	       
 	        	System.out.println("Switching to window: " + window);
 	            driver.switchTo().window(window);
 
