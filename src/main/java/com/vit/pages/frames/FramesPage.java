c3ca6688fe898b1d451f24e6fa44b76ea407c7a4
Vitaliy Rodyna <ukrdao@gmail.com>
2022-08-17 17:45:01 +0300

    public NestedFramesPage clickNestedFrames(){         driver.findElement(By.linkText("Nested Frames")).click();         return new NestedFramesPage(driver);     } }

--- src/main/java/com/vit/pages/frames/FramesPage.java
+++ src/main/java/com/vit/pages/frames/FramesPage.java
@@ -6,19 +6,19 @@ import org.openqa.selenium.WebDriver;
 
 public class FramesPage {
 
-private WebDriver driver;
+	private WebDriver driver;
 	
 	public FramesPage(WebDriver driver){
         this.driver = driver;
     }
-	
-	
 	
 	public NestedFramesPage clickNestedFrames(){
         driver.findElement(By.linkText("Nested Frames")).click();
         return new NestedFramesPage(driver);
     }
 }
+	
+	
     
 
     
