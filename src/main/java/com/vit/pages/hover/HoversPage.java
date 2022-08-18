c470464db06ee2a9ebf83f433304f0458584f769
Vitaliy Rodyna <ukrdao@gmail.com>
2022-08-18 16:08:30 +0300

    private By header = By.tagName("h5"); 		private By link = By.tagName("a");

--- src/main/java/com/vit/pages/hover/HoversPage.java
+++ src/main/java/com/vit/pages/hover/HoversPage.java
@@ -30,11 +30,11 @@ public class HoversPage {
 	public class FigureCaption{
 		
 		private WebElement caption;
-		
-		
-		
 		private By header = By.tagName("h5");
 		private By link = By.tagName("a");
+		
+		
+		
 
 		 public FigureCaption(WebElement caption){
 	            this.caption = caption;
