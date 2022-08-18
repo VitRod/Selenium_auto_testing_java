50ed1c53cdc822f726399985003690f853998af1
Vitaliy Rodyna <ukrdao@gmail.com>
2022-08-18 16:01:30 +0300

    private WebElement caption;

--- src/main/java/com/vit/pages/hover/HoversPage.java
+++ src/main/java/com/vit/pages/hover/HoversPage.java
@@ -28,7 +28,11 @@ public class HoversPage {
 	
        
 	public class FigureCaption{
+		
 		private WebElement caption;
+		
+		
+		
 		private By header = By.tagName("h5");
 		private By link = By.tagName("a");
 
